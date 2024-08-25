f81bf91f8fae00994567f5e920c0d534a3fddcbf
Phurich Thawornying <phurich.twy@gmail.com>
2024-08-26 03:14:52 +0700

    react done, css left

--- random-quote/src/app/page.js
+++ random-quote/src/app/page.js
@@ -1,16 +1,32 @@
+"use client";
 
+import { useState } from 'react';
+import quoteData from './quote.json';
 
 export default function Home() {
+  const [text, setText] = useState('');
+  const [author, setAuthor] = useState('');
+
+  function getQuote() {
+    const newQuote = quoteData.quotes[Math.floor(Math.random() * quoteData.quotes.length)];
+    setText(newQuote.quote);
+    setAuthor(newQuote.author);
+    console.log(text+author);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div id="quote-box">
-        <div id="text">lorem text </div>
-        <div id="author">author</div>
+    <main id="quote-box" className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <div id="text">{text}</div>
+        <div id="author">{author}</div>
       </div>
       <div>
-        <button id="new-quote">Get me new quote</button>
+        <button id="new-quote" onClick={getQuote}>Get me new quote</button>
         <a id="tweet-quote" target="_blank" href="twitter.com/intent/tweet">Tweet!</a>
       </div>
+      <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
     </main>
   );
 }
+
+
